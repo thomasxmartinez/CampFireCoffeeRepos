752ba7dcdc8f2b6207b20494df6422c6026a4bb2
Thomas Martinez <tommytnez@Thomass-MacBook-Air.local>
2016-10-03 14:05:48 -0700

    hopefully correct stuff for n6

--- cfc.js
+++ cfc.js
@@ -1,5 +1,9 @@
 'use strict'
 
+var months = ['Jan','Feb','Mar','Apr','Jun','Jul','Aug']
+var monthsEl = document.getElementById('months');
+
+
 var hrs = ['6:00AM','7:00AM','8:00AM','9:00AM','10:00AM','11:00AM','12:00PM','1:00PM','2:00PM','3:00PM','4:00PM','5:00PM','6:00','7:00PM','8:00PM']
 
 var pike = {
